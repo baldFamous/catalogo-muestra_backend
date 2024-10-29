d5a3a043b0a94b13c0ea7333d906b3e90a7874cc
brodriguez <bastian3967a@gmail.com>
2024-10-29 09:27:05 -0300

    project init

--- catalogo_backend/catalogo_backend/settings.py
+++ catalogo_backend/catalogo_backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bl*2ab$ma9vo6lunzdwp@5zjqm5s_ot&r&la+7^iuci&abkdvf'
+SECRET_KEY = 'key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
